1edb9f1e44266eb28050ce697ada7e83c340e5b7
Santosh Marella <smarella@apache.org>
2019-06-23 20:25:59 -0700

    HDFS-12914. Block report leases cause missing blocks until next report.  Contributed by Santosh Marella, He Xiaoqiao.
    Signed-off-by: Wei-Chiu Chuang <weichiu@apache.org>
    Co-authored-by: He Xiaoqiao <hexiaoqiao@apache.org>

--- hadoop-hdfs-project/hadoop-hdfs/src/main/java/org/apache/hadoop/hdfs/server/namenode/FSNamesystem.java
+++ hadoop-hdfs-project/hadoop-hdfs/src/main/java/org/apache/hadoop/hdfs/server/namenode/FSNamesystem.java
@@ -461,7 +461,7 @@ public class FSNamesystem implements Namesystem, FSNamesystemMBean,
 
   /** The namespace tree. */
   FSDirectory dir;
-  private final BlockManager blockManager;
+  private BlockManager blockManager;
   private final SnapshotManager snapshotManager;
   private final CacheManager cacheManager;
   private final DatanodeStatistics datanodeStatistics;
@@ -6233,6 +6233,11 @@ public class FSNamesystem implements Namesystem, FSNamesystemMBean,
   /** @return the block manager. */
   public BlockManager getBlockManager() {
     return blockManager;
+  }
+
+  @VisibleForTesting
+  public void setBlockManagerForTesting(BlockManager bm) {
+    this.blockManager = bm;
   }
 
   /** @return the FSDirectory. */
