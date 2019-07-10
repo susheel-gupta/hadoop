411c19c2f46beb484a9fb5b70ee54cbf30cd4c04
Steve Loughran <stevel@cloudera.com>
2019-07-10 20:57:02 +0100

    CDPD-2163. HADOOP-16393. S3Guard init command uses global settings, not those of target bucket.
    Contributed by Steve Loughran.
    
    Change-Id: I226a91ab8d7758340f8d221aa80a7abf9a0d3e8f
    (cherry picked from commit c7b5f858a00ece19e6ee22e9cb09080dfd419a56)

--- hadoop-tools/hadoop-aws/src/test/java/org/apache/hadoop/fs/s3a/s3guard/ITestS3GuardToolDynamoDB.java
+++ hadoop-tools/hadoop-aws/src/test/java/org/apache/hadoop/fs/s3a/s3guard/ITestS3GuardToolDynamoDB.java
@@ -53,6 +53,7 @@ import org.apache.hadoop.fs.s3a.s3guard.S3GuardTool.Init;
 import static org.apache.hadoop.fs.s3a.Constants.S3GUARD_DDB_REGION_KEY;
 import static org.apache.hadoop.fs.s3a.Constants.S3GUARD_DDB_TABLE_NAME_KEY;
 import static org.apache.hadoop.fs.s3a.Constants.S3GUARD_DDB_TABLE_TAG;
+import static org.apache.hadoop.fs.s3a.S3ATestUtils.removeBucketOverrides;
 import static org.apache.hadoop.fs.s3a.S3AUtils.setBucketOption;
 import static org.apache.hadoop.fs.s3a.s3guard.DynamoDBMetadataStore.*;
 import static org.apache.hadoop.fs.s3a.s3guard.S3GuardTool.*;
@@ -136,6 +137,11 @@ public class ITestS3GuardToolDynamoDB extends AbstractS3GuardToolTestBase {
 
     conf.set(S3GUARD_DDB_TABLE_NAME_KEY,
         getTestTableName("testDynamoTableTagging-" + UUID.randomUUID()));
+    String bucket = getFileSystem().getBucket();
+    removeBucketOverrides(bucket, conf,
+        S3GUARD_DDB_TABLE_NAME_KEY,
+        S3GUARD_DDB_REGION_KEY);
+
     S3GuardTool.Init cmdR = new S3GuardTool.Init(conf);
     Map<String, String> tagMap = new HashMap<>();
     tagMap.put("hello", "dynamo");
@@ -144,7 +150,7 @@ public class ITestS3GuardToolDynamoDB extends AbstractS3GuardToolTestBase {
     String[] argsR = new String[]{
         cmdR.getName(),
         "-tag", tagMapToStringParams(tagMap),
-        getFileSystem().getBucket()
+        "s3a://" + bucket + "/"
     };
 
     // run
