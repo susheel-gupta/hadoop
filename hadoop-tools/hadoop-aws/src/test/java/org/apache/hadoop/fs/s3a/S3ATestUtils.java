b8c420e2174d46974709d9ed0d97d8d115a61b09
Steve Loughran <stevel@cloudera.com>
2019-08-09 14:52:37 +0100

    CDPD-1477. HADOOP-16499. S3A retry policy to be exponential.
    Contributed by Steve Loughran.
    
    Change-Id: I261673f5585e0f6c8989e3ce7e8d339bd1afd87a

--- hadoop-tools/hadoop-aws/src/test/java/org/apache/hadoop/fs/s3a/S3ATestUtils.java
+++ hadoop-tools/hadoop-aws/src/test/java/org/apache/hadoop/fs/s3a/S3ATestUtils.java
@@ -755,6 +755,35 @@ public final class S3ATestUtils {
   }
 
   /**
+   * Remove any values from a bucket and the base values too.
+   * @param bucket bucket whose overrides are to be removed. Can be null/empty.
+   * @param conf config
+   * @param options list of fs.s3a options to remove
+   */
+  public static void removeBaseAndBucketOverrides(final String bucket,
+      final Configuration conf,
+      final String... options) {
+    for (String option : options) {
+      conf.unset(option);
+    }
+    removeBucketOverrides(bucket, conf, options);
+  }
+
+  /**
+   * Remove any values from the test bucket and the base values too.
+   * @param conf config
+   * @param options list of fs.s3a options to remove
+   */
+  public static void removeBaseAndBucketOverrides(
+      final Configuration conf,
+      final String... options) {
+    for (String option : options) {
+      conf.unset(option);
+    }
+    removeBaseAndBucketOverrides(getTestBucketName(conf), conf, options);
+  }
+
+  /**
    * Call a function; any exception raised is logged at info.
    * This is for test teardowns.
    * @param log log to use.
@@ -814,21 +843,6 @@ public final class S3ATestUtils {
   }
 
   /**
-   * Remove any values from a bucket and the base values too.
-   * @param bucket bucket whose overrides are to be removed. Can be null/empty.
-   * @param conf config
-   * @param options list of fs.s3a options to remove
-   */
-  public static void removeBaseAndBucketOverrides(final String bucket,
-      final Configuration conf,
-      final String... options) {
-    for (String option : options) {
-      conf.unset(option);
-    }
-    removeBucketOverrides(bucket, conf, options);
-  }
-
-  /*
    * Get a file status from S3A with the {@code needEmptyDirectoryFlag}
    * state probed.
    * This accesses a package-private method in the
