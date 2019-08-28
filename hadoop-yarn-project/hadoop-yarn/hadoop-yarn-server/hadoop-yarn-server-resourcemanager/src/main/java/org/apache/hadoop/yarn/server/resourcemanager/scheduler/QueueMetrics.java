0a6fc633631a669ce1c0c5210382221421b12053
Eric E Payne <ericp@verizonmedia.com>
2019-08-28 20:40:34 +0000

    YARN-9756: Create metric that sums total memory/vcores preempted per round. Contributed by  Manikandan R (manirajv06).
    Change-Id: Ida0200dd416bacbea9f276ff55fa61af69579b1b

--- hadoop-yarn-project/hadoop-yarn/hadoop-yarn-server/hadoop-yarn-server-resourcemanager/src/main/java/org/apache/hadoop/yarn/server/resourcemanager/scheduler/QueueMetrics.java
+++ hadoop-yarn-project/hadoop-yarn/hadoop-yarn-server/hadoop-yarn-server-resourcemanager/src/main/java/org/apache/hadoop/yarn/server/resourcemanager/scheduler/QueueMetrics.java
@@ -93,6 +93,10 @@ public class QueueMetrics implements MetricsSource {
 
   @Metric("App Attempt First Container Allocation Delay")
     MutableRate appAttemptFirstContainerAllocationDelay;
+  @Metric("Aggregate total of preempted memory MB")
+    MutableCounterLong aggregateMemoryMBPreempted;
+  @Metric("Aggregate total of preempted vcores")
+    MutableCounterLong aggregateVcoresPreempted;
 
   //Metrics updated only for "default" partition
   @Metric("Allocated memory in MB") MutableGaugeLong allocatedMB;
@@ -611,6 +615,23 @@ public class QueueMetrics implements MetricsSource {
     }
   }
 
+  public void updatePreemptedResources(Resource res) {
+    aggregateMemoryMBPreempted.incr(res.getMemorySize());
+    aggregateVcoresPreempted.incr(res.getVirtualCores());
+    if (parent != null) {
+      parent.updatePreemptedResources(res);
+    }
+  }
+
+  public void updatePreemptedForCustomResources(Resource res) {
+    if (queueMetricsForCustomResources != null) {
+      queueMetricsForCustomResources.increaseAggregatedPreempted(res);
+    }
+    if (parent != null) {
+      parent.updatePreemptedForCustomResources(res);
+    }
+  }
+
   public void updatePreemptedSecondsForCustomResources(Resource res,
           long seconds) {
     if (queueMetricsForCustomResources != null) {
@@ -776,6 +797,16 @@ public class QueueMetrics implements MetricsSource {
   @VisibleForTesting
   public MutableCounterLong getAggregateVcoreSecondsPreempted() {
     return aggregateVcoreSecondsPreempted;
+  }
+
+  @VisibleForTesting
+  public long getAggregateMemoryMBPreempted() {
+    return aggregateMemoryMBPreempted.value();
+  }
+
+  @VisibleForTesting
+  public long getAggregateVcoresPreempted() {
+    return aggregateVcoresPreempted.value();
   }
 
   public long getAllocatedMB() {
