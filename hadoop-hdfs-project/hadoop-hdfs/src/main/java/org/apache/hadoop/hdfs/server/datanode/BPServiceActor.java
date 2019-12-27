64694ab7f87b7066e118d81c9f5bb1aaa7f31611
Ayush Saxena <ayushsaxena@apache.org>
2019-12-27 09:12:44 +0530

    HDFS-14997. Addendum: BPServiceActor processes commands from NameNode asynchronously. Contributed by Xiaoqiao He.
    (cherry picked from commit 80f91d14ab0fb385252d4eeb19141bd059303d59)
    Change-Id: I6d0044da369cf6837bac9d8a8020040b854eefa3

--- hadoop-hdfs-project/hadoop-hdfs/src/main/java/org/apache/hadoop/hdfs/server/datanode/BPServiceActor.java
+++ hadoop-hdfs-project/hadoop-hdfs/src/main/java/org/apache/hadoop/hdfs/server/datanode/BPServiceActor.java
@@ -594,6 +594,9 @@ class BPServiceActor implements Runnable {
     if (bpThread != null) {
       bpThread.interrupt();
     }
+    if (commandProcessingThread != null) {
+      commandProcessingThread.interrupt();
+    }
   }
   
   //This must be called only by blockPoolManager
