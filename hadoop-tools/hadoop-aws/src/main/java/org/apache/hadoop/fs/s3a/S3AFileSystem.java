1b3596f899145a4705b0e19be734a5c0ac3c5152
Steve Loughran <stevel@cloudera.com>
2020-01-09 18:22:04 +0000

    CDPD-6776. HADOOP-16697. Tune/audit S3A authoritative mode.
    Contains:
    
    HADOOP-16474. S3Guard ProgressiveRenameTracker to mark destination
                  dirirectory as authoritative on success.
    HADOOP-16684. S3guard bucket info to list a bit more about
                  authoritative paths.
    HADOOP-16722. S3GuardTool to support FilterFileSystem.
    
    This patch improves the marking of newly created/import directory
    trees in S3Guard DynamoDB tables as authoritative.
    
    Specific changes:
    
     * Renamed directories are marked as authoritative if the entire
       operation succeeded (HADOOP-16474).
     * When updating parent table entries as part of any table write,
       there's no overwriting of their authoritative flag.
    
    s3guard import changes:
    
    * new -verbose flag to print out what is going on.
    
    * The "s3guard import" command lets you declare that a directory tree
    is to be marked as authoritative
    
      hadoop s3guard import -authoritative -verbose s3a://bucket/path
    
    When importing a listing and a file is found, the import tool queries
    the metastore and only updates the entry if the file is different from
    before, where different == new timestamp, etag, or length. S3Guard can get
    timestamp differences due to clock skew in PUT operations.
    
    As the recursive list performed by the import command doesn't retrieve the
    versionID, the existing entry may in fact be more complete.
    When updating an existing due to clock skew the existing version ID
    is propagated to the new entry (note: the etags must match; this is needed
    to deal with inconsistent listings).
    
    There is a new s3guard command to audit a s3guard bucket/path's
    authoritative state:
    
      hadoop s3guard authoritative -check-config s3a://bucket/path
    
    This is primarily for testing/auditing.
    
    The s3guard bucket-info command also provides some more details on the
    authoritative state of a store (HADOOP-16684).
    
    Change-Id: I58001341c04f6f3597fcb4fcb1581ccefeb77d91

--- hadoop-tools/hadoop-aws/src/main/java/org/apache/hadoop/fs/s3a/S3AFileSystem.java
+++ hadoop-tools/hadoop-aws/src/main/java/org/apache/hadoop/fs/s3a/S3AFileSystem.java
@@ -1222,7 +1222,8 @@ public class S3AFileSystem extends FileSystem implements StreamCapabilities,
     } catch (AmazonClientException e) {
       throw translateException("rename(" + src +", " + dst + ")", src, e);
     } catch (RenameFailedException e) {
-      LOG.debug(e.getMessage());
+      LOG.info("{}", e.getMessage());
+      LOG.debug("rename failure", e);
       return e.getExitCode();
     } catch (FileNotFoundException e) {
       LOG.debug(e.toString());
@@ -2454,7 +2455,7 @@ public class S3AFileSystem extends FileSystem implements StreamCapabilities,
    * @param path path
    * @return true if the path is auth
    */
-  protected boolean allowAuthoritative(final Path path) {
+  public boolean allowAuthoritative(final Path path) {
     return S3Guard.allowAuthoritative(path, this,
         allowAuthoritativeMetadataStore, allowAuthoritativePaths);
   }
@@ -2697,7 +2698,7 @@ public class S3AFileSystem extends FileSystem implements StreamCapabilities,
                 + " s3modtime={}; msModTime={} updating metastore",
                 path, s3ModTime, msModTime);
             return S3Guard.putAndReturn(metadataStore, s3AFileStatus,
-                instrumentation, ttlTimeProvider);
+                ttlTimeProvider);
           }
         }
       }
@@ -2732,13 +2733,12 @@ public class S3AFileSystem extends FileSystem implements StreamCapabilities,
       }
       // entry was found, save in S3Guard
       return S3Guard.putAndReturn(metadataStore, s3FileStatus,
-          instrumentation, ttlTimeProvider);
+          ttlTimeProvider);
     } else {
       // there was no entry in S3Guard
       // retrieve the data and update the metadata store in the process.
       return S3Guard.putAndReturn(metadataStore,
           s3GetFileStatus(path, key, probes, tombstones),
-          instrumentation,
           ttlTimeProvider);
     }
   }
@@ -3154,12 +3154,12 @@ public class S3AFileSystem extends FileSystem implements StreamCapabilities,
     HadoopExecutors.shutdown(unboundedThreadPool, LOG,
         THREAD_POOL_SHUTDOWN_DELAY_SECONDS, TimeUnit.SECONDS);
     unboundedThreadPool = null;
-    closeAutocloseables(LOG, credentials);
     cleanupWithLogger(LOG,
         metadataStore,
         instrumentation,
         delegationTokens.orElse(null),
         signerManager);
+    closeAutocloseables(LOG, credentials);
     delegationTokens = Optional.empty();
     signerManager = null;
     credentials = null;
@@ -3506,13 +3506,21 @@ public class S3AFileSystem extends FileSystem implements StreamCapabilities,
           activeState = stateToClose;
         }
         S3Guard.addAncestors(metadataStore, p, ttlTimeProvider, activeState);
+        final boolean isDir = objectRepresentsDirectory(key, length);
         S3AFileStatus status = createUploadFileStatus(p,
-            S3AUtils.objectRepresentsDirectory(key, length), length,
+            isDir, length,
             getDefaultBlockSize(p), username, eTag, versionId);
-        S3Guard.putAndReturn(metadataStore, status,
-            instrumentation,
-            ttlTimeProvider,
-            activeState);
+        if (!isDir) {
+          S3Guard.putAndReturn(metadataStore, status,
+              ttlTimeProvider,
+              activeState);
+        } else {
+          // this is a directory marker so put it as such.
+          status.setIsEmptyDirectory(Tristate.TRUE);
+          S3Guard.putAuthDirectoryMarker(metadataStore, status,
+              ttlTimeProvider,
+              activeState);
+        }
       }
     } catch (IOException e) {
       if (failOnMetadataWriteError) {
