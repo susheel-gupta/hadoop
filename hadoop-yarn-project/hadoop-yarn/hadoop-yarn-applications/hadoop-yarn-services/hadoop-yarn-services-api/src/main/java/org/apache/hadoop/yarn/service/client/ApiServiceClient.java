60bfd80ab9cd035eea565635fcb43dbc10d6ca5e
Szilard Nemeth <snemeth@apache.org>
2020-01-20 09:28:23 +0100

    COMPX-7844. YARN-8148. Update decimal values for queue capacities shown on queue status CLI. Contributed by Prabhu Joseph
    (cherry picked from commit 14d0f9a775086b2f0d174818c00c118c11f0c2b6)
    
     Conflicts:
    	hadoop-yarn-project/hadoop-yarn/hadoop-yarn-client/src/test/java/org/apache/hadoop/yarn/client/cli/TestYarnCLI.java
    
    Change-Id: I25efcebb45499d57c3a7fa1fd831b441be1b2468

--- hadoop-yarn-project/hadoop-yarn/hadoop-yarn-applications/hadoop-yarn-services/hadoop-yarn-services-api/src/main/java/org/apache/hadoop/yarn/service/client/ApiServiceClient.java
+++ hadoop-yarn-project/hadoop-yarn/hadoop-yarn-applications/hadoop-yarn-services/hadoop-yarn-services-api/src/main/java/org/apache/hadoop/yarn/service/client/ApiServiceClient.java
@@ -95,7 +95,7 @@ public class ApiServiceClient extends AppAdminClient {
 
   public ApiServiceClient() {
   }
-  
+
   public ApiServiceClient(Configuration c) throws Exception {
     serviceInit(c);
   }
