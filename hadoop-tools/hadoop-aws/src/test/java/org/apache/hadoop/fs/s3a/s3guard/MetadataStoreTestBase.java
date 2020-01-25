9201e41ae8c1e59ab5d25a5030cea2e8bdfbc426
Steve Loughran <stevel@cloudera.com>
2020-01-25 18:35:02 +0000

    CDPD-6413. HADOOP-16746. mkdirs and s3guard Authoritative mode.
    Contributed by Steve Loughran.
    
    This fixes two problems with S3Guard authoritative mode and
    the auth directory flags which are stored in DynamoDB.
    
    1. mkdirs was creating dir markers without the auth bit,
       forcing needless scans on newly created directories and
       files subsequently added; it was only with the first listStatus call
       on that directory that the dir would be marked as authoritative -even
       though it would be complete already.
    
    2. listStatus(path) would reset the authoritative status bit of all
       child directories even if they were already marked as authoritative.
    
    Issue #2 is possibly the most expensive, as any treewalk using listStatus
    (e.g globfiles) would clear the auth bit for all child directories before
    listing them. And this would happen every single time...
    essentially you weren't getting authoritative directory listings.
    
    For the curious, that the major bug was actually found during testing
    -we'd all missed it during reviews.
    
    A lesson there: the better the tests the fewer the bugs.
    
    Maybe also: something obvious and significant can get by code reviews.
    
    	modified:   hadoop-tools/hadoop-aws/src/main/java/org/apache/hadoop/fs/s3a/S3AFileSystem.java
    	modified:   hadoop-tools/hadoop-aws/src/main/java/org/apache/hadoop/fs/s3a/s3guard/BulkOperationState.java
    	modified:   hadoop-tools/hadoop-aws/src/main/java/org/apache/hadoop/fs/s3a/s3guard/DynamoDBMetadataStore.java
    	modified:   hadoop-tools/hadoop-aws/src/main/java/org/apache/hadoop/fs/s3a/s3guard/LocalMetadataStore.java
    	modified:   hadoop-tools/hadoop-aws/src/main/java/org/apache/hadoop/fs/s3a/s3guard/MetadataStore.java
    	modified:   hadoop-tools/hadoop-aws/src/main/java/org/apache/hadoop/fs/s3a/s3guard/NullMetadataStore.java
    	modified:   hadoop-tools/hadoop-aws/src/main/java/org/apache/hadoop/fs/s3a/s3guard/S3Guard.java
    	modified:   hadoop-tools/hadoop-aws/src/test/java/org/apache/hadoop/fs/s3a/ITestS3GuardWriteBack.java
    	modified:   hadoop-tools/hadoop-aws/src/test/java/org/apache/hadoop/fs/s3a/auth/ITestRestrictedReadAccess.java
    	modified:   hadoop-tools/hadoop-aws/src/test/java/org/apache/hadoop/fs/s3a/impl/TestPartialDeleteFailures.java
    	modified:   hadoop-tools/hadoop-aws/src/test/java/org/apache/hadoop/fs/s3a/s3guard/ITestDynamoDBMetadataStore.java
    	modified:   hadoop-tools/hadoop-aws/src/test/java/org/apache/hadoop/fs/s3a/s3guard/ITestDynamoDBMetadataStoreAuthoritativeMode.java
    	modified:   hadoop-tools/hadoop-aws/src/test/java/org/apache/hadoop/fs/s3a/s3guard/ITestDynamoDBMetadataStoreScale.java
    	modified:   hadoop-tools/hadoop-aws/src/test/java/org/apache/hadoop/fs/s3a/s3guard/ITestS3GuardFsck.java
    	modified:   hadoop-tools/hadoop-aws/src/test/java/org/apache/hadoop/fs/s3a/s3guard/MetadataStoreTestBase.java
    	modified:   hadoop-tools/hadoop-aws/src/test/java/org/apache/hadoop/fs/s3a/s3guard/TestS3Guard.java
    
    Change-Id: Ic3ffda13f2af2430afedd50fd657b595c83e90a7

--- hadoop-tools/hadoop-aws/src/test/java/org/apache/hadoop/fs/s3a/s3guard/MetadataStoreTestBase.java
+++ hadoop-tools/hadoop-aws/src/test/java/org/apache/hadoop/fs/s3a/s3guard/MetadataStoreTestBase.java
@@ -22,6 +22,7 @@ import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collection;
+import java.util.Collections;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
@@ -71,6 +72,8 @@ public abstract class MetadataStoreTestBase extends HadoopTestBase {
   static final String GROUP = null;
   private final long accessTime = 0;
   private static ITtlTimeProvider ttlTimeProvider;
+
+  private static final List<Path> EMPTY_LIST = Collections.emptyList();
 
   /**
    * Each test should override this.  Will use a new Configuration instance.
@@ -556,7 +559,7 @@ public abstract class MetadataStoreTestBase extends HadoopTestBase {
     dirMeta.setAuthoritative(true);
     dirMeta.put(new PathMetadata(
         makeFileStatus("/a1/b1/file_new", 100)));
-    ms.put(dirMeta, null);
+    ms.put(dirMeta, EMPTY_LIST, null);
 
     dirMeta = ms.listChildren(strToPath("/a1/b1"));
     assertListingsEqual(dirMeta.getListing(), "/a1/b1/file1", "/a1/b1/file2",
@@ -757,7 +760,7 @@ public abstract class MetadataStoreTestBase extends HadoopTestBase {
     if (!allowMissing()) {
       DirListingMetadata parentDirMd = ms.listChildren(strToPath(parentDir));
       parentDirMd.setAuthoritative(true);
-      ms.put(parentDirMd, null);
+      ms.put(parentDirMd, EMPTY_LIST, null);
     }
 
     ms.prune(MetadataStore.PruneMode.ALL_BY_MODTIME, time);
@@ -796,7 +799,7 @@ public abstract class MetadataStoreTestBase extends HadoopTestBase {
       // set parent dir as authoritative
       DirListingMetadata parentDirMd = ms.listChildren(strToPath(parentDir));
       parentDirMd.setAuthoritative(true);
-      ms.put(parentDirMd, null);
+      ms.put(parentDirMd, EMPTY_LIST, null);
 
       // prune the ms
       ms.prune(MetadataStore.PruneMode.ALL_BY_MODTIME, time);
@@ -828,7 +831,7 @@ public abstract class MetadataStoreTestBase extends HadoopTestBase {
     }
     DirListingMetadata dirMeta =
         new DirListingMetadata(strToPath(dirPath), metas, authoritative);
-    ms.put(dirMeta, null);
+    ms.put(dirMeta, EMPTY_LIST, null);
 
     if (!allowMissing()) {
       assertDirectorySize(dirPath, filenames.length);
@@ -1009,7 +1012,7 @@ public abstract class MetadataStoreTestBase extends HadoopTestBase {
     }
     DirListingMetadata dirMeta =
         new DirListingMetadata(strToPath(dirPath), metas, authoritative);
-    ms.put(dirMeta, null);
+    ms.put(dirMeta, EMPTY_LIST, null);
   }
 
   protected void createNewDirs(String... dirs)
