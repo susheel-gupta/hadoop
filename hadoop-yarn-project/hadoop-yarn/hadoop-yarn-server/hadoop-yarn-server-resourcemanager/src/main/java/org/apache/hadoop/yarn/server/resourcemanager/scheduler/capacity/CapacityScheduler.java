a8986aaf5f8b00bc30ff42873fd3336231dfb0b3
Prabhu Joseph <prabhujoseph@apache.org>
2020-03-19 12:48:30 +0530

    YARN-10034. Remove Allocation Tags from released container from Decommission node
    Contributed by Kyungwan Nam. Reviewed by Adam Antal.
    
    Change-Id: I5a42ac8c858ee6dc50537dcbfa4201700663405a

--- hadoop-yarn-project/hadoop-yarn/hadoop-yarn-server/hadoop-yarn-server-resourcemanager/src/main/java/org/apache/hadoop/yarn/server/resourcemanager/scheduler/capacity/CapacityScheduler.java
+++ hadoop-yarn-project/hadoop-yarn/hadoop-yarn-server/hadoop-yarn-server-resourcemanager/src/main/java/org/apache/hadoop/yarn/server/resourcemanager/scheduler/capacity/CapacityScheduler.java
@@ -2003,6 +2003,7 @@ public class CapacityScheduler extends
         super.completedContainer(container, SchedulerUtils
             .createAbnormalContainerStatus(container.getContainerId(),
                 SchedulerUtils.LOST_CONTAINER), RMContainerEventType.KILL);
+        node.releaseContainer(container.getContainerId(), true);
       }
 
       // Remove reservations, if any
