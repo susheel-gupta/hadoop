e095351dc58b079e03da32249709c86749d52e7d
Wei-Chiu Chuang <weichiu@cloudera.com>
2020-03-25 16:11:51 -0700

    HADOOP-16749. Configuration parsing of CDATA values are blank
    Signed-off-by: Jonathan Eagles <jeagles@gmail.com>
    (cherry picked from commit c4fb43c94259546f2c96b50ceae9cd3adb726166)
    (cherry picked from commit da45684185a24969963b169c13b322b29921dc82)
    Change-Id: Idb588873271d0b0f3ee0d739e96bc55699767557

--- hadoop-common-project/hadoop-common/src/main/java/org/apache/hadoop/conf/Configuration.java
+++ hadoop-common-project/hadoop-common/src/main/java/org/apache/hadoop/conf/Configuration.java
@@ -3345,6 +3345,7 @@ public class Configuration implements Iterable<Map.Entry<String,String>>,
         handleStartElement();
         break;
       case XMLStreamConstants.CHARACTERS:
+      case XMLStreamConstants.CDATA:
         if (parseToken) {
           char[] text = reader.getTextCharacters();
           token.append(text, reader.getTextStart(), reader.getTextLength());
