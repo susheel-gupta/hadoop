165e93adf9461995ec0991ed111ea672c9982d2c
Peter Bacsko <pbacsko@cloudera.com>
2021-01-14 19:00:43 +0100

    CDPD-21445 Zeppelin build failure in 7.1.6
    Temporarily the dependency jsonschema2pojo was removed. It's unclear why
    a different version of com.fasterxml.jackson.dataformat:jackson-dataformat-yaml
    appears on the Zeppelin side during the dependency convergence check.
    
    Only 2.10.5 is supposed to be visible. This is a temporary workaround and ideally
    we expect things to return to normal as soon as we figure out why this happens.
    
    Change-Id: Iaa5d8ad8fec3767cdc16aff34572f1a84dd66e40

--- hadoop-yarn-project/hadoop-yarn/hadoop-yarn-server/hadoop-yarn-server-resourcemanager/src/main/java/org/apache/hadoop/yarn/server/resourcemanager/scheduler/capacity/placement/GeneratePojos.java
+++ hadoop-yarn-project/hadoop-yarn/hadoop-yarn-server/hadoop-yarn-server-resourcemanager/src/main/java/org/apache/hadoop/yarn/server/resourcemanager/scheduler/capacity/placement/GeneratePojos.java
@@ -18,20 +18,20 @@
 
 package org.apache.hadoop.yarn.server.resourcemanager.scheduler.capacity.placement;
 
-import java.io.File;
+//import java.io.File;
 import java.io.IOException;
-import java.net.URL;
-import java.nio.file.Paths;
-
-import org.jsonschema2pojo.DefaultGenerationConfig;
-import org.jsonschema2pojo.GenerationConfig;
-import org.jsonschema2pojo.Jackson2Annotator;
-import org.jsonschema2pojo.SchemaGenerator;
-import org.jsonschema2pojo.SchemaMapper;
-import org.jsonschema2pojo.SchemaStore;
-import org.jsonschema2pojo.rules.RuleFactory;
-
-import com.sun.codemodel.JCodeModel;
+//import java.net.URL;
+//import java.nio.file.Paths;
+//
+//import org.jsonschema2pojo.DefaultGenerationConfig;
+//import org.jsonschema2pojo.GenerationConfig;
+//import org.jsonschema2pojo.Jackson2Annotator;
+//import org.jsonschema2pojo.SchemaGenerator;
+//import org.jsonschema2pojo.SchemaMapper;
+//import org.jsonschema2pojo.SchemaStore;
+//import org.jsonschema2pojo.rules.RuleFactory;
+//
+//import com.sun.codemodel.JCodeModel;
 
 /**
  * Helper class to re-generate java POJOs based on the JSON schema.
@@ -46,31 +46,31 @@ public final class GeneratePojos {
   }
 
   public static void main(String[] args) throws IOException {
-    JCodeModel codeModel = new JCodeModel();
-    URL schemaURL = Paths.get(
-        "src/main/json_schema/MappingRulesDescription.json").toUri().toURL();
-
-    GenerationConfig config = new DefaultGenerationConfig() {
-      @Override
-      public boolean isGenerateBuilders() {
-        return false;
-      }
-
-      @Override
-      public boolean isUsePrimitives() {
-          return true;
-      }
-    };
-
-    SchemaMapper mapper =
-        new SchemaMapper(
-            new RuleFactory(config,
-                new Jackson2Annotator(config),
-                new SchemaStore()),
-            new SchemaGenerator());
-
-    mapper.generate(codeModel, "ignore", TARGET_PACKAGE, schemaURL);
-
-    codeModel.build(new File("src/main/java"));
+//    JCodeModel codeModel = new JCodeModel();
+//    URL schemaURL = Paths.get(
+//        "src/main/json_schema/MappingRulesDescription.json").toUri().toURL();
+//
+//    GenerationConfig config = new DefaultGenerationConfig() {
+//      @Override
+//      public boolean isGenerateBuilders() {
+//        return false;
+//      }
+//
+//      @Override
+//      public boolean isUsePrimitives() {
+//          return true;
+//      }
+//    };
+//
+//    SchemaMapper mapper =
+//        new SchemaMapper(
+//            new RuleFactory(config,
+//                new Jackson2Annotator(config),
+//                new SchemaStore()),
+//            new SchemaGenerator());
+//
+//    mapper.generate(codeModel, "ignore", TARGET_PACKAGE, schemaURL);
+//
+//    codeModel.build(new File("src/main/java"));
   }
 }
