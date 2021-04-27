6601278aaa52ba3a24427866ba786c77a3ba3044
bilaharith <52483117+bilaharith@users.noreply.github.com>
2021-04-27 15:45:52 +0530

    CDPD-28549. HADOOP-17536. ABFS: Supporting customer provided encryption key (#2707)
    Note: Contains one file change to "ITestCustomerProvidedKey.java" from
    "CDPD-29718. HADOOP-17290. ABFS: Add Identifiers to Client Request Header"
    required for not breaking the build and was previously skipped.
    
    Contributed by bilahari t h
    
    Change-Id: I86216e755b81e9d14f5e87844d9fd58e8940560c

--- hadoop-tools/hadoop-azure/src/main/java/org/apache/hadoop/fs/azurebfs/constants/ConfigurationKeys.java
+++ hadoop-tools/hadoop-azure/src/main/java/org/apache/hadoop/fs/azurebfs/constants/ConfigurationKeys.java
@@ -191,6 +191,8 @@ public final class ConfigurationKeys {
 
   /** Setting this true will make the driver use it's own RemoteIterator implementation */
   public static final String FS_AZURE_ENABLE_ABFS_LIST_ITERATOR = "fs.azure.enable.abfslistiterator";
+  /** Server side encryption key */
+  public static final String FS_AZURE_CLIENT_PROVIDED_ENCRYPTION_KEY = "fs.azure.client-provided-encryption-key";
 
   /** End point of ABFS account: {@value}. */
   public static final String AZURE_ABFS_ENDPOINT = "fs.azure.abfs.endpoint";
