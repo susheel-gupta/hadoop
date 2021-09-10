8460bb3841ce6a2ca0a9479f0a103efaea982c12
Benjamin Teke <brumi1024@users.noreply.github.com>
2021-09-10 17:32:42 +0200

    COMPX-8416. YARN-10872. Replace getPropsWithPrefix calls in AutoCreatedQueueTemplate (#3396)
    Co-authored-by: Benjamin Teke <bteke@cloudera.com>
    (cherry picked from commit 971f1b8b0a37b4d3eafdecf705afa4f684253e26)
    Change-Id: I0214f67b855b01ccf89fe82cf8171257b6a40769

--- hadoop-yarn-project/hadoop-yarn/hadoop-yarn-server/hadoop-yarn-server-resourcemanager/src/main/java/org/apache/hadoop/yarn/server/resourcemanager/scheduler/capacity/CapacitySchedulerConfiguration.java
+++ hadoop-yarn-project/hadoop-yarn/hadoop-yarn-server/hadoop-yarn-server-resourcemanager/src/main/java/org/apache/hadoop/yarn/server/resourcemanager/scheduler/capacity/CapacitySchedulerConfiguration.java
@@ -1594,8 +1594,9 @@ public class CapacitySchedulerConfiguration extends ReservationSchedulerConfigur
    */
   public Map<String, Set<String>> getConfiguredNodeLabelsByQueue() {
     Map<String, Set<String>> labelsByQueue = new HashMap<>();
-    Map<String, String> schedulerEntries = getPropsWithPrefix(
-        CapacitySchedulerConfiguration.PREFIX);
+    Map<String, String> schedulerEntries =
+        getConfigurationProperties().getPropertiesWithPrefix(
+            CapacitySchedulerConfiguration.PREFIX);
 
     for (Map.Entry<String, String> propertyEntry
         : schedulerEntries.entrySet()) {
