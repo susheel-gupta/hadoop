275a318cf38f0bcdc9033d26755cef629d0fb4f4
Jack <jackwangcs@outlook.com>
2021-09-13 10:54:11 +0800

    CDPD-65475. YARN-10903. Fix the headroom check in ParentQueue and RegularContainerAllocator for DRF (#3352)
    Contributed by Jie Wang <jie.wang@hulu.com>
    
    (cherry picked from commit d8026e387e2b18a22f223488cf2504d8f7c7131f)
    Change-Id: I0b3cb317be52ca888de95e7cc339528fb2df44d9
    (cherry picked from commit 670159e8c1e33096f8585b4c055e57a217f9dba8)

--- hadoop-yarn-project/hadoop-yarn/hadoop-yarn-server/hadoop-yarn-server-resourcemanager/src/main/java/org/apache/hadoop/yarn/server/resourcemanager/scheduler/capacity/AbstractParentQueue.java
+++ hadoop-yarn-project/hadoop-yarn/hadoop-yarn-server/hadoop-yarn-server-resourcemanager/src/main/java/org/apache/hadoop/yarn/server/resourcemanager/scheduler/capacity/AbstractParentQueue.java
@@ -961,10 +961,9 @@ public abstract class AbstractParentQueue extends AbstractCSQueue {
     // Two conditions need to meet when trying to allocate:
     // 1) Node doesn't have reserved container
     // 2) Node's available-resource + killable-resource should > 0
-    boolean accept = node.getReservedContainer() == null && Resources
-        .greaterThanOrEqual(resourceCalculator, clusterResource, Resources
-            .add(node.getUnallocatedResource(),
-                node.getTotalKillableResources()), queueAllocationSettings.getMinimumAllocation());
+    boolean accept = node.getReservedContainer() == null &&
+        Resources.fitsIn(resourceCalculator, queueAllocationSettings.getMinimumAllocation(),
+            Resources.add(node.getUnallocatedResource(), node.getTotalKillableResources()));
     if (!accept) {
       ActivitiesLogger.QUEUE.recordQueueActivity(activitiesManager, node,
           getParentName(), getQueueName(), ActivityState.REJECTED,
