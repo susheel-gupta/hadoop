698351541402806bf979bfd8c3fd0d8eda51a826
Mehakmeet Singh <mehakmeet.singh@cloudera.com>
2022-06-28 22:02:48 +0530

    CDPD-40050. HADOOP-18242. ABFS Rename Failure when tracking metadata is in an incomplete state (#4331)
    ABFS rename fails intermittently when the Storage-blob tracking
    metadata is in an incomplete state. This surfaces as the error code
    404 and an error message of "RenameDestinationParentPathNotFound"
    
    To mitigate this issue, when a request fails with this response.
    the ABFS client issues a HEAD call on the source file
    and then retries the rename operation again
    
    ABFS filesystem statistics track when this occurs with new counters
      rename_recovery
      metadata_incomplete_rename_failures
      rename_path_attempts
    
    This is very rare occurrence and appears to be triggered under certain
    heavy load conditions, just as with HADOOP-18163.
    
    Contributed by Mehakmeet Singh.
    
    Change-Id: I8f6c2f3f2add16b9c88121fe0282e308aaa54191

--- hadoop-tools/hadoop-azure/src/main/java/org/apache/hadoop/fs/azurebfs/AzureBlobFileSystem.java
+++ hadoop-tools/hadoop-azure/src/main/java/org/apache/hadoop/fs/azurebfs/AzureBlobFileSystem.java
@@ -1575,7 +1575,7 @@ public class AzureBlobFileSystem extends FileSystem
   }
 
   @VisibleForTesting
-  AzureBlobFileSystemStore getAbfsStore() {
+  public AzureBlobFileSystemStore getAbfsStore() {
     return abfsStore;
   }
 
