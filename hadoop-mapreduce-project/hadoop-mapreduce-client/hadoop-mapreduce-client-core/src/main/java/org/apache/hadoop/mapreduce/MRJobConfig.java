460825fb6edf2ab2098d9893c48b03c11c55de9a
9uapaw <gyorandr@gmail.com>
2022-08-31 17:30:24 +0200

    CDPD-43912. MAPREDUCE-7409. Make shuffle key length configurable. Contributed by Ashutosh Gupta.
    (cherry picked from commit 84081a8cae7d050cc3dc2b3bddd298d769032120)
    Change-Id: I1db0472cf3b6a5af8fd113833d690a5c5dce5583
    (cherry picked from commit 5641dd50aef94d3e8d7d8bad9189904054163ebb)

--- hadoop-mapreduce-project/hadoop-mapreduce-client/hadoop-mapreduce-client-core/src/main/java/org/apache/hadoop/mapreduce/MRJobConfig.java
+++ hadoop-mapreduce-project/hadoop-mapreduce-client/hadoop-mapreduce-client-core/src/main/java/org/apache/hadoop/mapreduce/MRJobConfig.java
@@ -1232,4 +1232,6 @@ public interface MRJobConfig {
    */
   @Unstable
   String INPUT_FILE_MANDATORY_PREFIX = "mapreduce.job.input.file.must.";
+  String SHUFFLE_KEY_LENGTH = "mapreduce.shuffle-key-length";
+  int DEFAULT_SHUFFLE_KEY_LENGTH = 64;
 }
