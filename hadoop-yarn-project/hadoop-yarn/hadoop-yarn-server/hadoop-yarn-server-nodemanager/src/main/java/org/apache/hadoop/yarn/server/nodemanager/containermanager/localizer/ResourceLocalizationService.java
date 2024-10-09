7fc91d074b25fc9e6ffe3f7ace9bba724f68c4a8
zmolnar <zmolnar@cloudera.com>
2024-10-09 13:21:19 +0200

    CDPD-75272 YARN-11709. NodeManager should be marked unhealthy on localizer config issues (#7043) (#159)
    (cherry picked from commit d1311e52f783bf96d5d0eac9cc0bc7c281dc8964)
    
    Co-authored-by: Benjamin Teke <brumi1024@users.noreply.github.com>
    (cherry picked from commit 468b7fcda0d8c3e8dd1c28cb913d51dd730c83ff)

--- hadoop-yarn-project/hadoop-yarn/hadoop-yarn-server/hadoop-yarn-server-nodemanager/src/main/java/org/apache/hadoop/yarn/server/nodemanager/containermanager/localizer/ResourceLocalizationService.java
+++ hadoop-yarn-project/hadoop-yarn/hadoop-yarn-server/hadoop-yarn-server-nodemanager/src/main/java/org/apache/hadoop/yarn/server/nodemanager/containermanager/localizer/ResourceLocalizationService.java
@@ -20,6 +20,7 @@ package org.apache.hadoop.yarn.server.nodemanager.containermanager.localizer;
 import static org.apache.hadoop.fs.CreateFlag.CREATE;
 import static org.apache.hadoop.fs.CreateFlag.OVERWRITE;
 
+import org.apache.hadoop.yarn.exceptions.ConfigurationException;
 import org.apache.hadoop.yarn.server.nodemanager.recovery.RecoveryIterator;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -1260,10 +1261,13 @@ public class ResourceLocalizationService extends CompositeService
           throw new IOException("All disks failed. "
               + dirsHandler.getDisksHealthReport(false));
         }
-      // TODO handle ExitCodeException separately?
-      } catch (FSError fe) {
-        exception = fe;
-      } catch (Exception e) {
+        // TODO handle ExitCodeException separately?
+      } catch (ConfigurationException e) {
+        exception = e;
+        LOG.error("Failed to launch localizer for " + localizerId + ", due to configuration error. " +
+            "Marking the node unhealthy.", e);
+        nmContext.getNodeStatusUpdater().reportException(e);
+      } catch (Exception | FSError e) {
         exception = e;
       } finally {
         if (exception != null) {
