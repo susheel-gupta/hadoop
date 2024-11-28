4f4c5dcd70972cef75a379e4894fd4c0ac5e0a3d
Steve Loughran <stevel@cloudera.com>
2024-11-28 13:19:11 +0000

    CDPD-76378. HADOOP-19317. S3A: fs.s3a.connection.expect.continue controls 100 CONTINUE behavior (#7134) (#175)
    New option
    
      fs.s3a.connection.expect.continue
    
    This controls whether or not an PUT request to the S3 store
    sets the "Expect: 100-continue" header and awaits a 100 CONTINUE
    response before uploading any data.
    
    This allows for throttling and other problems to be detected fast.
    
    The default is "true" -the header is sent.
    
    (This is the SDK v1 backport).
    
    Contributed by Steve Loughran
    
    Change-Id: Ic448407268b80d2532c7b7080a21b0fe48694ef5

--- hadoop-tools/hadoop-aws/src/test/java/org/apache/hadoop/fs/s3a/ITestS3AConfiguration.java
+++ hadoop-tools/hadoop-aws/src/test/java/org/apache/hadoop/fs/s3a/ITestS3AConfiguration.java
@@ -610,4 +610,23 @@ public class ITestS3AConfiguration {
         conf, "Expected a connection error for " + MULTIPART_COPY_THRESHOLD);
   }
 
+  @Test(timeout = 10_000L)
+  public void testExpectContinueFalse() throws Exception {
+    Configuration config = new Configuration(false);
+    config.setBoolean(CONNECTION_EXPECT_CONTINUE, false);
+    ClientConfiguration awsConf = new ClientConfiguration();
+    initConnectionSettings(config, awsConf);
+    Assertions.assertThat(awsConf.isUseExpectContinue())
+        .describedAs("expect continue flag")
+        .isFalse();
+  }
+
+  @Test(timeout = 10_000L)
+  public void testExpectContinueDefault() throws Exception {
+    ClientConfiguration awsConf = new ClientConfiguration();
+    initConnectionSettings(new Configuration(false), awsConf);
+    Assertions.assertThat(awsConf.isUseExpectContinue())
+        .describedAs("expect continue flag")
+        .isTrue();
+  }
 }
